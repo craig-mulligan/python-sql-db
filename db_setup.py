6b945957061eae1091ab606b7a9ccdb745b9b597
Craig Mulligan <craig@firingsuad.co.za>
2015-09-04 11:48:10 +0300

    asdf

--- db_setup.py
+++ db_setup.py
@@ -14,8 +14,8 @@ class Reading(Base):
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    reading = Column(Integer, nullable=False)
-    timestamp = Column(DateTime, default=datetime.datetime.now)
+    reading = Column(Float, nullable=False)
+    stamp = Column(DateTime, default=datetime.datetime.now)
     
     # This will be used when we implement an API that returns json
     @property
