d3fc81b84d3a1aefa5c0f0dd4b10ffb3177e6666
Craig Mulligan <craig@firingsuad.co.za>
2015-09-04 11:26:22 +0300

    asdf

--- read.py
+++ read.py
@@ -8,7 +8,9 @@ def log():
 	session.add(newLog)
 	session.commit()
 	print "new log!"
-	print session.query(Reading).all()
+	readings = session.query(Reading).all()
+	for r in readings:
+		print "CPU: " + r.reading + "stamp: " + r.timestamp
 
 if __name__ == '__main__':
     log()
